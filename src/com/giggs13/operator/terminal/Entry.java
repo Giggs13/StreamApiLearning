898bad8485061f339296014dfe2d083d4660894b
Malykhin Andrej <Andrej.Malykhin@privatbank.ua>
2018-10-24 09:28:49 +0300

    some code

--- src/com/giggs13/operator/terminal/Entry.java
+++ src/com/giggs13/operator/terminal/Entry.java
@@ -1,5 +1,10 @@
 package com.giggs13.operator.terminal;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -9,6 +14,9 @@ public class Entry {
         runForEach​Ordered();
         runCount​();
         runCollect();
+        runToArray();
+        runReduce();
+        runMinAndMax();
     }
 
     static void runForEach​() {
@@ -55,6 +63,81 @@ public class Entry {
 
     static void runCollect() {
         // R collect​(Collector collector)
+        List<Integer> list1 = Stream.of(1, 2, 3)
+                .collect(Collectors.toList());
+        System.out.println(list1);
 
+        String s = Stream.of(1, 2, 3)
+                .map(String::valueOf)
+                .collect(Collectors.joining("-", "<", ">"));
+        System.out.println(s);
+
+        // R collect​(Supplier supplier, BiConsumer accumulator, BiConsumer combiner)
+        List<String> list2 = Stream.of("a", "b", "c", "d")
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        System.out.println(list2);
+
+        String concat = Stream.of("a", "b", "c", "d")
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .toString();
+        System.out.println(concat);
+    }
+
+    static void runToArray() {
+        // Object[] toArray​()
+        // A[] toArray​(IntFunction<A[]> generator)
+        Object[] elements1 = Stream.of("a", "b", "c", "d")
+                .toArray();
+        System.out.println(Arrays.toString(elements1));
+
+        String[] elements2 = Stream.of("a1", "b1", "c1", "d1")
+                .toArray(String[]::new);
+        System.out.println(Arrays.toString(elements2));
+    }
+
+    static void runReduce() {
+        // T reduce​(T identity, BinaryOperator accumulator)
+        // U reduce​(U identity, BiFunction accumulator, BinaryOperator combiner)
+        int sum1 = Stream.of(1, 2, 3, 4, 5)
+                .reduce(10, (acc, x) -> acc * x);
+        System.out.println(sum1);
+
+        Integer sum2 = IntStream.of(1, 2, 3, 4)
+                .reduce(100, (a, b) -> a + b);
+        System.out.println(sum2);
+
+        Integer sum3 = IntStream.of(1, 2, 3, 4)
+                .reduce(100, Integer::sum);
+        System.out.println(sum3);
+
+        // Optional reduce​(BinaryOperator accumulator)
+        Optional<Integer> result = Stream.<Integer>empty()
+                .reduce((acc, x) -> acc + x);
+        System.out.println(result);
+        System.out.println(result.isPresent());
+
+        Optional<Integer> sum = Stream.of(1, 2, 3, 4, 5)
+                .reduce((acc, x) -> acc + x);
+        System.out.println(sum);
+        System.out.println(sum.isPresent());
+        System.out.println(sum.get());
+
+        int product = IntStream.range(0, 10)
+                .filter(x -> x++ % 4 == 0)
+                .reduce((acc, x) -> acc * x)
+                .getAsInt();
+        System.out.println(product);
+    }
+
+    static void runMinAndMax() {
+        // Optional min​(Comparator comparator)
+        int min = Stream.of(20, 11, 45, 78, 13)
+                .min(Integer::compare).get();
+        System.out.println(min);
+
+        // Optional max​(Comparator comparator)
+        int max = Stream.of(20, 11, 45, 78, 13)
+                .max(Integer::compare).get();
+        System.out.println(max);
     }
 }
